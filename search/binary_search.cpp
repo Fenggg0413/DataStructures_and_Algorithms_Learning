db818cb41fb9274fc7bb8dc9bc68076e91323b57
Feng <18178712792@163.com>
2023-07-12 17:00:38 +0800

    search

---
+++ search/binary_search.cpp
@@ -0,0 +1,81 @@
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+using namespace chrono;
+
+int binarySearch(vector<int> &vec, int target)
+{
+    int beg = 0, end = vec.size() - 1;
+    while (beg <= end)
+    {
+        int mid = beg + (end - beg) / 2;
+        if (target > vec[mid])
+        {
+            beg = mid + 1;
+        }
+        else if (target < vec[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int normalSearch(vector<int> &vec, int target)
+{
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        if (vec[i] == target)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    uniform_int_distribution<unsigned> u(0,10000);
+    default_random_engine e;
+    vector<int> v;
+    v.reserve(10000000);
+    for (int i = 0; i != 10000000; ++i)
+    {
+        v.push_back(u(e));
+    }
+
+    {
+        auto start = system_clock::now();
+        int i = normalSearch(v, 86);
+        auto end = system_clock::now();
+        auto duration = duration_cast<microseconds>(end - start);
+        if (i > -1)
+            cout << "Successfully found value: " << v[i] << endl;
+        else
+            cout << "The value does not exist." << endl;
+        cout << "Normal search has cost: "
+             << double(duration.count()) * microseconds::period::num / microseconds::period::den
+             << "seconds" << endl;
+    }
+
+    {
+        sort(v.begin(), v.end());
+        auto start = system_clock::now();
+        int i = binarySearch(v, 54);
+        auto end = system_clock::now();
+        auto duration = duration_cast<microseconds>(end - start);
+        if (i > -1)
+            cout << "Successfully found value: " << v[i] << endl;
+        else
+            cout << "The value does not exist." << endl;
+        cout << "Binary search has cost: "
+             << double(duration.count()) * microseconds::period::num / microseconds::period::den
+             << "seconds" << endl;
+    }
+}
