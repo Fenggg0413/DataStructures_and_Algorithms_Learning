18ac948fef2c0657d16b9e8b2de469a71b7ef883
Fen4 <18178712792@163.com>
2023-07-19 23:27:41 +0800

    update quick_sort model

--- leetcodeAC/quick_sort.cpp
+++ leetcodeAC/quick_sort.cpp
@@ -7,23 +7,16 @@ int a[N];
 
 void quick_sort(int a[], int l, int r)
 {
-    if (l >= r)
-        return;
-
-    int x = a[l], i = l - 1, j = r + 1;
-    while (i < j)
+    if(l >= r)  return;
+    int i = l-1, j = r+1, x = a[(l+r) >> 1];
+    while(i < j)
     {
-        do
-            ++i;
-        while (a[i] < x);
-        do
-            --j;
-        while (a[j] > x);
-        if (i < j)
-            swap(a[i], a[j]);
+        do  ++i;while(a[i] < x);
+        do  --j;while(a[j] > x);
+        if(i < j)   swap(a[i], a[j]);
     }
     quick_sort(a, l, j);
-    quick_sort(a, j + 1, r);
+    quick_sort(a, j+1, r);
 }
 
 int main()
@@ -34,6 +27,6 @@ int main()
         scanf("%d", &a[i]);
     quick_sort(a, 0, n - 1);
     for (int i = 0; i != n; ++i)
-        printf("%d", a[i]);
+        printf("%d ", a[i]);
     return 0;
 }
