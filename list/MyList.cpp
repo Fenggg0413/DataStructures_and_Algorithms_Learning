a06d271d6553ca81e9d23a33aab1927b774402a6
Fen4 <18178712792@163.com>
2023-09-18 14:57:36 +0800

    9-18 learning

---
+++ list/MyList.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+class MyLinkedList
+{
+public:
+    struct ListNode
+    {
+        int val;
+        ListNode *next;
+        ListNode(int x) : val(x), next(nullptr) {}
+    };
+    MyLinkedList()
+    {
+        size = 0;
+        dummyhead = new ListNode(0);
+    }
+
+    int get(int index)
+    {
+        if (index >= size || index < 0)
+        {
+            return -1;
+        }
+        ListNode *cur = dummyhead->next;
+        while (index--)
+        {
+            cur = cur->next;
+        }
+        return cur->val;
+    }
+
+    void addAtHead(int val)
+    {
+        ListNode *newHead = new ListNode(val);
+        newHead->next = dummyhead->next;
+        dummyhead->next = newHead;
+        ++size;
+    }
+
+    void addAtTail(int val)
+    {
+        ListNode *cur = dummyhead;
+        while (cur->next)
+            cur = cur->next;
+        cur->next = new ListNode(val);
+        ++size;
+    }
+
+    void addAtIndex(int index, int val)
+    {
+        if (index > size) // index = size时相当于尾插
+            return;
+        if (index < 0)
+            index = 0;
+        ListNode *cur = dummyhead;
+        while (index--)
+        {
+            cur = cur->next;
+        }
+        ListNode *p = new ListNode(val);
+        p->next = cur->next;
+        cur->next = p;
+        ++size;
+    }
+
+    void deleteAtIndex(int index)
+    {
+        if (index >= size || index < 0)
+            return;
+        else
+        {
+            ListNode *cur = dummyhead;
+            while (index--)
+            {
+                cur = cur->next;
+            }
+            ListNode *tmp = cur->next;
+            cur->next = cur->next->next;
+            delete tmp;
+            // delete命令指示释放了tmp指针原本所指的那部分内存，
+            // 被delete后的指针tmp的值（地址）并非就是NULL，而是随机值。也就是被delete后，
+            // 如果不再加上一句tmp=nullptr,tmp会成为乱指的野指针
+            // 如果之后的程序不小心使用了tmp，会指向难以预想的内存空间
+            tmp = nullptr;
+            --size;
+        }
+    }
+
+private:
+    ListNode *dummyhead;
+    int size;
+};
+
+int main()
+{
+    MyLinkedList list;
+    list.addAtHead(1);
+    list.addAtHead(2);
+    list.addAtTail(3);
+    cout << list.get(1) << endl; //1
+    list.deleteAtIndex(1);
+    cout << list.get(1) << endl; //3
+    return 0;
+}
