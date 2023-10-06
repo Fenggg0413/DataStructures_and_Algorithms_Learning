7ba9481d6b90e024175e55bf2d7371b8eb89008b
Feng <18178712792@163.com>
2023-10-06 14:39:39 +0800

    lab1

---
+++ test/lab.c
@@ -0,0 +1,403 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR 0
+#define OK 1
+#define Overflow 2   // 上溢
+#define Underflow 3  // 下溢
+#define NotPresent 4 // 元素不存在
+#define Duplicate 5  // 有重复元素
+
+typedef int Status;   // 状态
+typedef int ElemType; // 自定义类型
+
+// 顺序表的定义
+typedef struct
+{
+    int n;
+    int maxLength;
+    ElemType *element;
+} SeqList;
+
+// 顺序表的初始化
+Status Init(SeqList *L, int mSize)
+{
+    L->maxLength = mSize;
+    L->n = 0;
+    L->element = (ElemType *)malloc(sizeof(ElemType) * mSize);
+    if (!L->element)
+        return ERROR;
+    return OK;
+}
+
+// 顺序表的查找
+Status Find(SeqList L, int i, ElemType *x)
+{
+    if (i < 0 || i > L.n - 1)
+        return ERROR;
+    *x = L.element[i];
+    return OK;
+}
+
+// 顺序表的元素插入
+Status Insert(SeqList *L, int i, ElemType x)
+{
+    if (i < -1 || i > L->n - 1)
+        return ERROR;
+    if (L->n == L->maxLength)
+        return ERROR;
+    for (int j = L->n - 1; j > i; j--)
+    {
+        L->element[j + 1] = L->element[j];
+    }
+    L->element[i + 1] = x;
+    L->n++;
+    return OK;
+}
+
+// 顺序表的元素删除
+Status Delete(SeqList *L, int i)
+{
+    if (i < -1 || i > L->n - 1)
+        return ERROR;
+    if (!L->n)
+        return ERROR;
+    for (int j = i + 1; j < L->n; j++)
+    {
+        L->element[j - 1] = L->element[j];
+    }
+    L->n--;
+    return OK;
+}
+
+// 顺序表的输出
+Status Output(SeqList *L)
+{
+    if (!L->n)
+        return ERROR;
+    for (int i = 0; i < L->n; i++)
+    {
+        printf("%d ", L->element[i]);
+    }
+    printf("\n");
+    return OK;
+}
+
+// 顺序表的撤销
+Status Destroy(SeqList *L)
+{
+    L->n = 0;
+    L->maxLength = 0;
+    free(L->element);
+}
+
+// 链表节点定义
+typedef struct node
+{
+    ElemType element;
+    struct node *next;
+} Node;
+
+// 链表定义
+typedef struct LinkList
+{
+    Node *dummyhead; // 虚拟头节点
+    int n;
+} LinkList;
+
+// 链表初始化
+Status LinkListInit(LinkList *L)
+{
+    L->n = 0;
+    L->dummyhead = (Node *)malloc(sizeof(Node));
+    L->dummyhead->next = NULL;
+    L->dummyhead->element = 0;
+    return OK;
+}
+
+// 链表元素的查找
+Status LinkListFind(LinkList *L, int i, ElemType *x)
+{
+    if (i < 0 || i > L->n - 1)
+        return ERROR;
+    Node *cur = L->dummyhead->next;
+    while (i--)
+        cur = cur->next;
+    *x = cur->element;
+    return OK;
+}
+
+// 链表元素的插入
+Status LinkListInsert(LinkList *L, int i, ElemType x)
+{
+    if (i > L->n)
+        return ERROR;
+    if (i < 0) // i<0相当于头插
+        i = 0;
+    Node *cur = L->dummyhead;
+    while (i--)
+        cur = cur->next;
+    Node *q = (Node *)malloc(sizeof(Node));
+    q->element = x;
+    q->next = cur->next;
+    cur->next = q;
+    L->n++;
+    return OK;
+}
+
+// 链表元素的删除
+Status LinkListDelete(LinkList *L, int i)
+{
+    if (i < 0 || i > L->n - 1)
+        return ERROR;
+    Node *cur = L->dummyhead;
+    while (i--)
+    {
+        cur = cur->next;
+    }
+    Node *temp = cur->next;
+    cur->next = cur->next->next;
+    free(temp);
+    temp = NULL;
+    L->n--;
+}
+
+// 链表的输出
+Status LinkListOutput(LinkList *L)
+{
+    if (!L->n)
+        return ERROR;
+    Node *cur = L->dummyhead->next;
+    while (cur)
+    {
+        printf("%d ", cur->element);
+        cur = cur->next;
+    }
+    printf("\n");
+    return OK;
+}
+
+// 链表的撤销
+Status LinkListDestroy(LinkList *L)
+{
+    L->n = 0;
+    Node *cur;
+    while (cur)
+    {
+        cur = L->dummyhead->next;
+        free(L->dummyhead);
+        L->dummyhead = cur;
+    }
+    return OK;
+}
+
+// 链表的逆置
+Status reverse(LinkList *L)
+{
+    if (!L->n)
+        return ERROR;
+    Node *cur = L->dummyhead->next;
+    Node *tmp = cur;
+    Node *head = L->dummyhead;
+    head = NULL;
+    while (cur)
+    {
+        cur = cur->next;
+        tmp->next = head;
+        head = tmp;
+        tmp = cur;
+    }
+    L->dummyhead->next = head;
+    return OK;
+}
+
+// 链表的排序
+Status Sort(LinkList *L)
+{
+    if (!L->n)
+        return ERROR;
+    Node *min, *cur, *tmp;
+    tmp = L->dummyhead;
+    for (int i = 0; i < L->n - 1; i++)
+    {
+        tmp = tmp->next;
+        min = tmp;
+        cur = tmp;
+        for (int j = i; j < L->n; j++)
+        {
+            if (cur->element < min->element)
+                min = cur;
+            cur = cur->next;
+        }
+        // 将每次找到的最小值交换到暂存节点处
+        ElemType val = min->element;
+        min->element = tmp->element;
+        tmp->element = val;
+    }
+    return OK;
+}
+
+// 多项式计算
+typedef struct pNode
+{
+    int coef;
+    int exp;
+    struct PNode *next;
+} PNode;
+typedef struct polynominal
+{
+    PNode *head;
+} Polynominal;
+
+// 多项式的创建
+void create(Polynominal *P)
+{
+    PNode *pn, *pre, *q;
+    P->head = (PNode *)malloc(sizeof(PNode));
+    P->head->exp = -1;
+    P->head->next = P->head;
+    for (;;)
+    {
+        pn = (PNode *)malloc(sizeof(PNode));
+        printf("系数: ");
+        scanf("%d", &pn->coef);
+        printf("指数: ");
+        scanf("%d", &pn->exp);
+        if (pn->exp < 0) // 当出现输入指数为负数时，多项式创建结束
+            break;
+        pre = P->head;
+        q = P->head->next;
+        while (q && q->exp > pn->exp)
+        {
+            pre = q;
+            q = q->next;
+        }
+        pn->next = q;
+        pre->next = pn;
+    }
+}
+
+// 多项式的输出
+void printInfo(Polynominal *p)
+{
+    PNode *cur = p->head->next;
+    while (cur != p->head)
+    {
+        if (cur->coef != 0)
+        {
+            if (cur->exp != 0)
+            {
+                if (cur->coef == 1)
+                    printf("X^%d", cur->exp);
+                else if (cur->coef == -1)
+                    printf("-X^%d", cur->exp);
+                else
+                    printf("%dX^%d", cur->coef, cur->exp);
+            }
+            else
+                printf("%d", cur->coef);
+            PNode *flag = cur->next;
+            if (flag != p->head)
+            {
+                if (flag->coef > 0)
+                    printf("+");
+            }
+        }
+        cur = cur->next;
+    }
+}
+
+// 多项式的撤销
+void pDestroy(Polynominal *p)
+{
+    PNode *cur = p->head->next;
+    while (cur != p->head)
+    {
+        PNode *tmp = cur;
+        cur = cur->next;
+        free(tmp);
+        tmp = NULL;
+    }
+    free(p->head);
+    p->head = NULL;
+}
+
+// 多项式相加
+void addPoly(Polynominal *px, Polynominal *qx)
+{
+    PNode *p, *q1, *q;
+    p = px->head->next;
+    q1 = qx->head;
+    q = qx->head->next;
+    while (p != px->head)
+    {
+        while (p->exp < q->exp)
+        {
+            q1 = q;
+            q = q->next;
+        }
+        if (p->exp == q->exp)
+        {
+            q->coef = q->coef + p->coef;
+            if(q->coef == 0)
+            {
+                q1->next = q->next;
+                free(q);
+                q = q1->next;
+                p = p->next;
+            }
+            else
+            {
+                q1 = q;
+                q = q->next;
+                p = p->next;
+            }
+        }
+        else
+        {
+            PNode *temp = (PNode *)malloc(sizeof(PNode));
+            temp->coef = p->coef;
+            temp->exp = p->exp;
+            temp->next = q1->next;
+            q1->next = temp;
+            q1 = q1->next;
+            p = p->next;
+        }
+    }
+}
+
+int main()
+{
+    SeqList list;
+    Init(&list, 10);
+    for (int i = 0; i < 10; i++)
+        Insert(&list, i - 1, i);
+    printf("对顺序表操作\n");
+    Output(&list);
+    Delete(&list, 0);
+    Output(&list);
+    Destroy(&list);
+    printf("---------------------------------------\n");
+    LinkList ll;
+    LinkListInit(&ll);
+    for (int i = 0; i < 10; i++)
+        LinkListInsert(&ll, i, i);
+    printf("对链表操作\n");
+    LinkListOutput(&ll);
+    printf("删除链表中第1个元素后:\n");
+    LinkListDelete(&ll, 0);
+    LinkListOutput(&ll);
+    printf("链表逆置后: \n");
+    reverse(&ll);
+    LinkListOutput(&ll);
+    printf("链表排序后: \n");
+    Sort(&ll);
+    LinkListOutput(&ll);
+    LinkListDestroy(&ll);
+    printf("---------------------------------------\n");
+    Polynominal p1;
+    create(&p1);
+    printInfo(&p1);
+    pDestroy(&p1);
+    return 0;
+}
