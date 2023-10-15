e0abec1f196cc15f123ad7168c3c4d2e6789a9ce
Fen4 <18178712792@163.com>
2023-10-15 22:48:10 +0800

    linklist practise

--- leetcodeAC/leetcode142.cpp
+++ leetcodeAC/leetcode142.cpp
@@ -6,10 +6,30 @@ struct ListNode
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
-class Solution //TODO:142循环链表
+class Solution // TODO:142循环链表
 {
 public:
     ListNode *detectCycle(ListNode *head)
     {
+        // 引入虚拟头节点避免head为null的情况
+        ListNode *dummyhead = new ListNode(0);
+        dummyhead->next = head;
+        ListNode *fast = dummyhead, *slow = dummyhead;
+        while (fast->next != nullptr && fast->next->next != nullptr)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) // 如果快指针与慢指针相遇，则说明有环
+            {
+                ListNode *index1 = dummyhead, *index2 = fast;
+                while (index1 != index2)
+                {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index1;
+            }
+        }
+        return nullptr;
     }
 };
