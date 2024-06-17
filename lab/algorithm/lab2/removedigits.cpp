6d8011413b5c5ca0bb70edbcc551d83fb921f2c2
Feng <18178712792@163.com>
2024-06-17 14:49:21 +0800

    optmize removedigits

--- lab/algorithm/lab2/removedigits.cpp
+++ lab/algorithm/lab2/removedigits.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
-#include <stack>
 #include <string>
 
 using namespace std;
 
 string removeKDigits(string num, int k) {
-  int len = num.size();
-  if (k == len)
-    return "0"; // 如果需要删除的数量等于数字长度，直接返回0
-  stack<char> s;
-  for (int i = 0; i < len; ++i) {
-    // 如果当前字符小于栈顶元素且删除次数未达到k，则弹出栈顶元素
-    while (!s.empty() && s.top() > num[i] && k > 0) {
-      s.pop();
+  int n = num.size();
+  std::string result = "";
+
+  for (char digit : num)
+  {
+    while (k > 0 && !result.empty() && result.back() > digit)
+    {
+      result.pop_back();
       k--;
     }
-    s.push(num[i]);
+    result.push_back(digit);
   }
-  // 如果删除次数不足k，则继续从栈顶删除元素
-  while (k > 0) {
-    s.pop();
+
+  // 如果还有未删除的数字，从末尾删除
+  while (k > 0)
+  {
+    result.pop_back();
     k--;
-  }
-  // 构造结果字符串
-  string result = "";
-  while (!s.empty()) {
-    result = s.top() + result;
-    s.pop();
   }
   // 移除前导零
   while (result.size() > 1 && result[0] == '0') {
